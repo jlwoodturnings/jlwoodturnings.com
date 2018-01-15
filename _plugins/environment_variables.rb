9fba9dcb5ea46f13e733ef8de1f564c3c9b7000e
Josh Lesch <josh.r.lesch@gmail.com>
2018-01-14 23:17:20 -0600

    getform

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -4,6 +4,7 @@ module Jekyll
     class EnvironmentVariablesGenerator < Generator
         def generate(site)      
           site.config['google_id'] = ENV['GOOGLE_ANALYTICS_ID']
+          site.config['getform_token'] = ENV['GETFORM_TOKEN']
         end
     end
 end
